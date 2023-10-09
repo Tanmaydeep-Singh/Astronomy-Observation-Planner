3940067be9e4275f733c29dbee71fa3006926976
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2023-10-09 22:58:57 +0530

    dev: api updates

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,8 +1,14 @@
+import axios from 'axios'
 import React from 'react'
 
 function index() {
+
+  const starChart = async() => {
+  }
   return (
-    <div>UPCOMMING EVENTS LIST</div>
+    <div>UPCOMMING EVENTS LIST
+      <button onClick={ () => { starChart()}}>BUTTON</button>
+    </div>
   )
 }
 
